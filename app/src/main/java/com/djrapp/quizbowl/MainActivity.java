3a17f866fc783241c57eff55b133fa53760afe5f
ProjectBM <qud@bxscience.edu>
2017-12-13 11:52:23 -0500

    Start screen.

--- app/src/main/java/com/djrapp/quizbowl/MainActivity.java
+++ app/src/main/java/com/djrapp/quizbowl/MainActivity.java
@@ -1,7 +1,10 @@
 package com.djrapp.quizbowl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,19 +14,29 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private Button createRoom;
+    private Button joinRoom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+        createRoom = findViewById(R.id.create);
+        joinRoom = findViewById(R.id.join);
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+        createRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        joinRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChooseTeam.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
