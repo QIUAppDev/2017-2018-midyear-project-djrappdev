9d2f0d614e91e84a5da579bbbe6a4970ba275ef2
ProjectBM <qud@bxscience.edu>
2017-12-14 11:55:39 -0500

    temp

--- app/src/main/java/com/djrapp/quizbowl/ChooseTeamActivity.java
+++ app/src/main/java/com/djrapp/quizbowl/ChooseTeamActivity.java
@@ -2,13 +2,19 @@ package com.djrapp.quizbowl;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class ChooseTeamActivity extends AppCompatActivity {
+
+    Button createTeam, joinTeam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_team);
+
+        createTeam = findViewById(R.id.createteam);
+        joinTeam = findViewById(R.id.jointeam);
 
     }
 }
