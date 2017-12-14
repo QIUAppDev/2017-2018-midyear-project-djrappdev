a2096b3a8606fa3c0d1610108c77408d8919efdb
ProjectBM <qud@bxscience.edu>
2017-12-14 11:57:06 -0500

    not done

--- app/src/main/java/com/djrapp/quizbowl/ChooseTeamActivity.java
+++ app/src/main/java/com/djrapp/quizbowl/ChooseTeamActivity.java
@@ -1,7 +1,9 @@
 package com.djrapp.quizbowl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class ChooseTeamActivity extends AppCompatActivity {
@@ -15,6 +17,21 @@ public class ChooseTeamActivity extends AppCompatActivity {
 
         createTeam = findViewById(R.id.createteam);
         joinTeam = findViewById(R.id.jointeam);
+
+        createTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseTeamActivity.this, .class);
+                startActivity(intent);
+            }
+        });
+        joinTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseTeamActivity.this, .class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
